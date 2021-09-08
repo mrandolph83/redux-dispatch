1394d4bf00b35fe6ec3afc04e0e8bb610770a8d1
mrandolph83 <mrandolph83@gmail.com>
2021-09-08 12:12:32 -0400

    Assignment complete

--- js/reducer.js
+++ js/reducer.js
@@ -7,7 +7,22 @@ function changeState(state, action){
   }
 }
 
+function dispatch(action) {
+  state = changeState(state, action);
+return state;
+}
+
 let state = {count: 0}
 let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+function render() {
+  document.body.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+dispatch({ type: "INCREASE_COUNT" });
+dispatch({ type: "INCREASE_COUNT" });
